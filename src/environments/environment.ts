9919bb3c6ecaa004fdc31abe369d21ecba745b90
yond1994 <yond1994@gmail.com>
2020-09-30 18:54:06 +0200

    base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiurl: 'https://apimembers.racksnstacks.com'
-  // apiurl: 'http://localhost:3000',
+ // apiurl: 'https://apimembers.racksnstacks.com'
+   apiurl: 'http://localhost:3000',
 };
 // apiurl: 'http://localhost:3000'
 /*
