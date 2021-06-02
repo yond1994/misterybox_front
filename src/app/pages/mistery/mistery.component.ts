491005287eb6fe845491cec00a57267657d008b2
yond1994 <yond1994@gmail.com>
2021-06-02 22:47:20 +0200

    nuevo sorteo

--- src/app/pages/mistery/mistery.component.ts
+++ src/app/pages/mistery/mistery.component.ts
@@ -65,7 +65,7 @@ export class MisteryComponent implements OnInit {
     }
     this.clock = this.source.subscribe(t => {
       this.now = new Date();
-      this.end = new Date('05/02/' + 2021 + ' 23:00');
+      this.end = new Date('06/02/' + 2021 + ' 23:00');
       // this.end = new Date('01/01/' + (this.now.getFullYear()) + ' 23:00');
       this.showDate();
     });
